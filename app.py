9d2291369e7b7818f4b54556bba8758103c9972f
Keerthi-Vijaibabu <keerthi.vijaibabu@gmail.com>
2025-04-29 11:23:52 +0530

    app routing completed

--- app.py
+++ app.py
@@ -1,27 +1,49 @@
-from flask import Flask, render_template, request, redirect
-import mysql.connector
-
+from flask import Flask, render_template, request, redirect, session
+from db import cursor
 app = Flask(__name__)
+# Add secret key for session
+app.secret_key = 'f9a8f7c3c9e14b8a3fa5dc092bfea3b2a4ccdef26d27fae1dc82a9db9c14fd21'
 
-# Database Connection
-db = mysql.connector.connect(
-    host="localhost",
-    user="your_mysql_username",
-    password="your_mysql_password",
-    database="expense_db"
-)
-cursor = db.cursor()
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        query = "Select password from users where username =", username
+        cursor.execute(query)
+        password_correct = cursor.fetchone
+        if password == password_correct:
+            session['user'] = username
+            return redirect('/')
+        else:
+            error = 'Invalid username or password'
+
+    return render_template('login.html', error=error)
+
+# Logout Route
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect('/login')
 
 # Home Route (view all expenses)
 @app.route('/')
 def index():
+    if 'user' not in session:
+        return redirect('/login')
+    
     cursor.execute("SELECT * FROM expenses")
     expenses = cursor.fetchall()
     return render_template('index.html', expenses=expenses)
 
-# Add Expense Route (handle form submission)
+# Add Expense Route
 @app.route('/add', methods=['POST'])
 def add_expense():
+    if 'user' not in session:
+        return redirect('/login')
+    
     amount = request.form['amount']
     category = request.form['category']
     description = request.form['description']
